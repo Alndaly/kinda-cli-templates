84d449b213916551be3fc80cea14b32e88c05994
华勤达 <hqd01334772@antgroup.com>
2022-10-09 21:34:50 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import styles from  './index.scss'
+
+ReactDOM.render(
+    <h1 className={styles.container}>Hello, world! </h1>,
+    document.querySelector('.root')
+);
